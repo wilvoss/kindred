cfb716a99d7fa56a0628034fbafffce4334529bf
wilvoss <wilv@allovus.com>
2025-07-14 08:57:18 -0700

    1.0.018 - og image

--- scripts/kindred.js
+++ scripts/kindred.js
@@ -14,7 +14,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '1.0.017',
+    currentVersion: '1.0.018',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
