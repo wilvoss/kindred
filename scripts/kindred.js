2b05282560a95b25b40090f21f1b6fb4a9d24f84
wilvoss <wilv@allovus.com>
2025-07-13 17:50:39 -0700

    1.0.003 - simplified herald

--- scripts/kindred.js
+++ scripts/kindred.js
@@ -14,7 +14,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '1.0.002',
+    currentVersion: '1.0.003',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
