d397864e1929bba08cbdcb465ad3d2d9f8edecdd
wilvoss <wilv@allovus.com>
2025-07-13 17:47:18 -0700

    1.0.002 - herald face size

--- scripts/kindred.js
+++ scripts/kindred.js
@@ -14,7 +14,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '1.0.001',
+    currentVersion: '1.0.002',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
