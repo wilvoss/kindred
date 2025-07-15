0c7b511b16073936f159373f2f9599e9c19a2c05
wilvoss <wilv@allovus.com>
2025-07-15 08:36:23 -0700

    1.0.025 - updated icons

--- scripts/kindred.js
+++ scripts/kindred.js
@@ -14,7 +14,7 @@ var app = new Vue({
   data: {
     serviceWorker: '',
     storedVersion: 0,
-    currentVersion: '1.0.024',
+    currentVersion: '1.0.025',
     deviceHasTouch: true,
     allPlayerRanks: AllPlayerRanks,
     currency: new Currency(),
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.024';
+const CACHE_VERSION = '1.0.025';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
