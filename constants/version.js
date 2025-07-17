f11acf1fff9bf8de996fe5853d1a25d9b4564a40
wilvoss <wilv@allovus.com>
2025-07-17 14:36:13 -0700

    1.0.035 - sw check

--- constants/version.js
+++ constants/version.js
@@ -1,1 +1,1 @@
-export const version = '1.0.034';
+export const version = '1.0.035';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.034';
+const CACHE_VERSION = '1.0.035';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
